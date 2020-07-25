3604fb153fdc9e1806f31476bdd4f3c766a50538
KyleMueller <s1054831@monmouth.edu>
2020-07-24 23:27:34 -0400

    finsihed 9

--- ProjectEulerJavaSolutions/src/Project009.java
+++ ProjectEulerJavaSolutions/src/Project009.java
@@ -10,19 +10,33 @@ public class Project009 {
 
 	public static void main(String[] args) {
 		int ans = 0;
+		boolean flag = false;
 		for(int i = 1; i < 1000; i++) {
 			for (int j = 1; j < 1000; j++) {
-				if((i*i)+(j*j) > 1000) {
+				if((i)+(j) > 1000) {
+					break;
+				}
+				if(flag) {
 					break;
 				}
 				for (int k = 1; k < 1000; k++) {
-					if((i*i)+(j*j)+(k*k) > 1000) {
+					if((i)+(j)+(k) > 1000) {
 						break;
+					}
+					else if ((i)+(j)+(k) == 1000) {
+						if((i*i)+(j*j) == (k*k)) {
+							ans = i*j*k;
+							flag = true;
+						}
 					}
 					
 				}
 			}
+			if(flag) {
+				break;
+			}
 		}
+		System.out.println(ans);
 	}
 
 }
