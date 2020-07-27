42206492c8133a39d7860e34fb6cbcc95cfca154
KyleMueller <s1054831@monmouth.edu>
2020-07-26 23:52:05 -0400

    proj 16 completed (in voice of c engineer)

---
+++ ProjectEulerJavaSolutions/src/Project016.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+public class Project016 {
+	/**
+	 * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+		What is the sum of the digits of the number 2^1000?
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigInteger num = new BigInteger("2");
+		num = num.pow(1000);
+		String str = num.toString();
+		int sum = 0;
+		for(int i = 0; i < str.length(); i++) {
+			String sub = str.substring(i,i+1);
+			sum = sum + Integer.parseInt(sub);
+		}
+		System.out.println(sum);
+	}
+
+}
+
