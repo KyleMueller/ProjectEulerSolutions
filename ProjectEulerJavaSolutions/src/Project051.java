63cfe3924513ead9c02a721c29f646b08a2be5be
KyleMueller <s1054831@monmouth.edu>
2020-09-13 00:28:08 -0400

    finished 51

--- ProjectEulerJavaSolutions/src/Project051.java
+++ ProjectEulerJavaSolutions/src/Project051.java
@@ -6,7 +6,7 @@ public class Project051 {
 	static Map<Integer, Boolean> primes = new HashMap<Integer, Boolean>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int limit = 10000000;
+		int limit = 1000000;
 		boolean prime[] = new boolean[limit+1]; 
         for(int i=0; i < limit; i++) { 
             prime[i] = true;
@@ -23,7 +23,6 @@ public class Project051 {
         		primes.put(i, true);
         	}
         }
-        
         for(int i = 10; i < limit; i++) {
         	if(primes.containsKey(i)) {
         		if(check(i)) {
@@ -32,9 +31,13 @@ public class Project051 {
         		}
         	}
         }
+        //i don't agree with the wording of this question
+        //but replace 1's in where 1 is in the binary and you
+        //get the right answer of 121313
         
-		
-
+        //simply put, it want's the smallest prime of the family
+        //program outputs the smallest prime that can produce the family
+        
 	}
 	public static boolean check(int A) {
 		int len = (A + "").length();
@@ -43,7 +46,24 @@ public class Project051 {
 		for(int i = 1; i <= upperBinLim; i++) {
 			String tempBin = getBinary(i);
 			int trueCount = 0;
+			int count1s = 0;
+			boolean firstDigOne = false;
+			for(int k = 0; k < tempBin.length(); k++) {
+				//can't have a number that only swaps 1 value
+				if(tempBin.charAt(k) == '1') {
+					count1s++;
+				}
+				if(k == 0 && tempBin.charAt(k) == '1') {
+					firstDigOne = true;
+				}
+			}
+			if(count1s < 2) {
+				continue;
+			}
 			for(int j = 0; j < 10; j++) {
+				if(j == 0 && firstDigOne) {
+					continue;
+				}
 				if(primes.containsKey(convert(A, tempBin, j))) {
 					trueCount++;
 				}
@@ -51,7 +71,8 @@ public class Project051 {
 					break;
 				}
 			}
-			if(trueCount >= 8) {
+			if(trueCount == 8) {
+				System.out.println(tempBin);
 				return true;
 			}
 		}	
